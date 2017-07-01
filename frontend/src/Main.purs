ca12b9dd1cb9beab46ca0dee93f07c3c3d774842
Athan Clark <athan.clark@gmail.com>
2017-07-01 17:23:09 -0600

    hello-world pux

--- frontend/src/Main.purs
+++ frontend/src/Main.purs
@@ -3,7 +3,40 @@ module Main where
 import Prelude
 import Control.Monad.Eff (Eff)
 import Control.Monad.Eff.Console (CONSOLE, log)
+import Control.Monad.Eff.Exception (EXCEPTION)
 
-main :: forall e. Eff (console :: CONSOLE | e) Unit
+import Pux (start, noEffects, FoldP)
+import Pux.Renderer.React (renderToDOM)
+import Text.Smolder.Markup (Markup, text)
+import Signal (Signal)
+import Signal.Channel (CHANNEL)
+
+
+data State = State
+
+initialState :: State
+initialState = State
+
+data Event = Event
+
+foldp :: forall eff. FoldP State Event eff
+foldp Event State = noEffects State
+
+view :: forall handler. State -> Markup handler
+view State = text ":D"
+
+inputs :: Array (Signal Event)
+inputs = []
+
+
+main :: forall e
+      . Eff ( console   :: CONSOLE
+            , channel   :: CHANNEL
+            , exception :: EXCEPTION
+            | e) Unit
 main = do
   log "Hello sailor!"
+
+  app <- start
+    {initialState, foldp, view, inputs}
+  renderToDOM "#app" app.markup app.input
