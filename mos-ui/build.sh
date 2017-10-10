1d0a96e8c15cc648bbfb9b90737baf2955e65f2a
Athan Clark <athan.clark@gmail.com>
2017-10-10 17:37:04 -0600

    updated versions, don't need to bundle actually

--- mos-ui/build.sh
+++ mos-ui/build.sh
@@ -1,6 +1,6 @@
 #! /bin/bash
 
 ./node_modules/.bin/pulp build && \
-    purs bundle output/**/*.js -m Spec --main Spec > frontend.js && \
-    purs bundle output/**/*.js -m Main --main Main > index.js && \
+    # purs bundle output/**/*.js -m Spec --main Spec > frontend.js && \
+    # purs bundle output/**/*.js -m Main --main Main > index.js && \
     ./node_modules/.bin/electron-packager . --overwrite
